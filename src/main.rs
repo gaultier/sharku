35f1f91be3a06aa4cd4a90e121a85837ce6409e8
Philippe Gaultier <philippe.gaultier@ppro.com>
2021-10-01 15:52:22 +0200

    add serde

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,80 @@
-use anyhow::{Context, Result};
-use bendy::decoding::FromBencode;
-use bendy::decoding::Object;
-use bendy::decoding::{Error, ResultExt};
-use std::{fs::File, io::Read};
+extern crate serde;
+extern crate serde_bencode;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_bytes;
 
-#[derive(Debug, Eq, PartialEq)]
-struct TorrentFile {
-    announce: String,
+use anyhow::{Context, Result};
+use serde_bencode::de;
+use serde_bytes::ByteBuf;
+use std::fs::File as F;
+use std::io::Read;
+
+#[derive(Debug, Deserialize)]
+struct Node(String, i64);
+
+#[derive(Debug, Deserialize)]
+struct File {
+    path: Vec<String>,
+    length: i64,
+    #[serde(default)]
+    md5sum: Option<String>,
 }
 
-impl FromBencode for TorrentFile {
-    const EXPECTED_RECURSION_DEPTH: usize = 20;
+#[derive(Debug, Deserialize)]
+struct Info {
+    name: String,
+    pieces: ByteBuf,
+    #[serde(rename = "piece length")]
+    piece_length: i64,
+    #[serde(default)]
+    md5sum: Option<String>,
+    #[serde(default)]
+    length: Option<i64>,
+    #[serde(default)]
+    files: Option<Vec<File>>,
+    #[serde(default)]
+    private: Option<u8>,
+    #[serde(default)]
+    path: Option<Vec<String>>,
+    #[serde(default)]
+    #[serde(rename = "root hash")]
+    root_hash: Option<String>,
+}
 
-    fn decode_bencode_object(object: Object) -> std::result::Result<Self, Error> {
-        let mut dict = object.try_into_dictionary()?;
-
-        let mut announce: Option<String> = None;
-        while let Some(pair) = dict.next_pair()? {
-            match pair {
-                (b"announce", value) => {
-                    announce = String::decode_bencode_object(value)
-                        .context("announce")
-                        .map(Some)?;
-                    break;
-                }
-                _ => continue,
-            }
-        }
-
-        Ok(TorrentFile {
-            announce: announce.ok_or_else(|| Error::missing_field("announce"))?,
-        })
-    }
+#[derive(Debug, Deserialize)]
+struct Torrent {
+    info: Info,
+    #[serde(default)]
+    announce: Option<String>,
+    #[serde(default)]
+    nodes: Option<Vec<Node>>,
+    #[serde(default)]
+    encoding: Option<String>,
+    #[serde(default)]
+    httpseeds: Option<Vec<String>>,
+    #[serde(default)]
+    #[serde(rename = "announce-list")]
+    announce_list: Option<Vec<Vec<String>>>,
+    #[serde(default)]
+    #[serde(rename = "creation date")]
+    creation_date: Option<i64>,
+    #[serde(rename = "comment")]
+    comment: Option<String>,
+    #[serde(default)]
+    #[serde(rename = "created by")]
+    created_by: Option<String>,
 }
 
 fn main() -> Result<()> {
-    let mut f = File::open("openbsd.torrent").context("Failed to open torrent file")?;
+    let mut f = F::open("openbsd.torrent").context("Failed to open torrent file")?;
     let mut content = Vec::with_capacity(100_000);
     f.read_to_end(&mut content)
         .context("Failed to read torrent file")?;
 
-    let torrent_file = TorrentFile::from_bencode(&content);
+    let torrent_file =
+        de::from_bytes::<Torrent>(&content).context("Failed to parse torrent file")?;
 
-    println!("{:?}", torrent_file);
+    println!("{:#?}", torrent_file);
     Ok(())
 }
