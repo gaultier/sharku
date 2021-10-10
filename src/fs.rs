a57ef6ffb02611ed5c14c4928ca30a906b2fa25a
Philippe Gaultier <philippe.gaultier@ppro.com>
2021-10-10 11:10:24 +0200

    improve errors

--- src/fs.rs
+++ src/fs.rs
@@ -4,10 +4,14 @@ use std::fs::File;
 use std::path::Path;
 
 pub fn open(path: &Path, size: u64) -> Result<MmapMut> {
-    let file = File::create(path).with_context(|| "Failed to open file as RW")?;
+    let file = File::create(path)
+        .with_context(|| format!("Failed to open file as RW: path={}", path.to_string_lossy()))?;
     file.set_len(size)
         .with_context(|| format!("Failed to truncate to size={}", size))?;
-    let mmap = unsafe { Mmap::map(&file).with_context(|| "Failed to mmap")? };
+    let mmap = unsafe {
+        Mmap::map(&file)
+            .with_context(|| format!("Failed to mmap: path={}", path.to_string_lossy()))?
+    };
     mmap.make_mut()
         .with_context(|| "Failed to obtain a mutable file mmap")
 }
