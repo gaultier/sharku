9e3e71dcdb5cca7e7cfd43098aeab28ac8f51a2d
Philippe Gaultier <philippe.gaultier@ppro.com>
2021-10-10 14:44:36 +0200

    fix opening file

--- src/fs.rs
+++ src/fs.rs
@@ -1,17 +1,33 @@
 use anyhow::*;
-use memmap::{Mmap, MmapMut};
-use std::fs::File;
+use memmap::MmapMut;
+use std::fs::OpenOptions;
 use std::path::Path;
 
 pub fn open(path: &Path, size: u64) -> Result<MmapMut> {
-    let file = File::create(path)
-        .with_context(|| format!("Failed to open file as RW: path={}", path.to_string_lossy()))?;
-    file.set_len(size)
-        .with_context(|| format!("Failed to truncate to size={}", size))?;
+    let file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(&path)
+        .with_context(|| {
+            format!(
+                "Failed to open file as RW: path={} len={}",
+                path.to_string_lossy(),
+                size
+            )
+        })?;
+
+    file.set_len(size).with_context(|| {
+        format!(
+            "Failed to open file as RW: path={} len={}",
+            path.to_string_lossy(),
+            size
+        )
+    })?;
+
     let mmap = unsafe {
-        Mmap::map(&file)
+        MmapMut::map_mut(&file)
             .with_context(|| format!("Failed to mmap: path={}", path.to_string_lossy()))?
     };
-    mmap.make_mut()
-        .with_context(|| "Failed to obtain a mutable file mmap")
+    Ok(mmap)
 }
